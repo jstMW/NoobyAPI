250136ea886d6d33985efbd5abb6dba4105667c7
jst-MW <unknown>
2024-12-23 17:23:06 -0600

    add: first design structure, gradle

---
+++ lib/src/main/java/org/Prototype/Prototype.java
@@ -0,0 +1,13 @@
+/*
+ * This is just a working prototype, hopefully.
+*/
+
+package org.Prototype;
+
+import java.net.URL;
+
+public class Prototype {
+    public static main(String[] args) {
+    }
+
+}
