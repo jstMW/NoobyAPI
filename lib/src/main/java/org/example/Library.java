a3e5b989297b376493f3193993a2e169f77e8ea4
jst-MW <unknown>
2024-12-24 09:00:46 -0600

    add: a sample of 2 methods and associated test to experiment

--- lib/src/main/java/org/example/Library.java
+++ lib/src/main/java/org/example/Library.java
@@ -3,8 +3,36 @@
  */
 package org.example;
 
+import java.net.http.*;
+import java.net.URI;
+import java.util.Base64;
+
 public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+
+    public Library() {
+    };
+
+    public HttpRequest buildHttpRequest(String uri, String authVal) {
+
+        return HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(uri))
+                .header("Authorization", authVal)
+                .build();
+    }
+
+    public String buildAuthVal(String apiKey) {
+        // as apears in the api docs
+        StringBuilder authVal = new StringBuilder("Basic ");
+        // followd by Base64 of the apiKey
+        byte[] apiKeyAsByteArray = apiKey.getBytes();
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encodedAPIKey = encoder.encodeToString(apiKeyAsByteArray);
+        // return the authVal
+        return authVal.append(encodedAPIKey).toString();
+    }
+
 }
--- lib/src/test/java/org/example/LibraryTest.java
+++ lib/src/test/java/org/example/LibraryTest.java
@@ -6,9 +6,62 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.http.HttpRequest;
+import java.net.URI;
+import java.net.http.HttpHeaders;
+import java.util.List;
+import java.util.Map;
+
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
+    @Test
+    void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
         assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
     }
+
+    @Test
+    void buildAuthValTest() {
+        Library classUnderTest = new Library();
+        String apiKey = "Some Random String of char";
+        String expectied_AuthVal = "Basic U29tZSBSYW5kb20gU3RyaW5nIG9mIGNoYXI=";
+        assertEquals(expectied_AuthVal, classUnderTest.buildAuthVal(apiKey));
+    }
+
+    @Test
+    void buildHttpRequest() {
+        // setup initial condition
+        Library classUnderTest = new Library();
+        String url = "https://example.com/api/test/me";
+        String authVal = "authentication value";
+        HttpRequest request = classUnderTest.buildHttpRequest(url, authVal);
+
+        // setup expeted values for uri test
+        URI uri = request.uri();
+        String expectedScheme = "https://";
+        String expectedHost = "example.com";
+        String expectedPath = "/api/test/me";
+        int expectedPort = 443;
+
+        // setup expeted values for header test
+        int expectedNumOfHeaders = 1;
+        int expectedNumOfAuthHeaderValue = 1;
+        String expectedAuthHeaderValue = authVal;
+
+        // test uri
+        assertEquals(expectedScheme, uri.getScheme());
+        assertEquals(expectedHost, uri.getHost());
+        assertEquals(expectedPath, uri.getPath());
+        assertEquals(expectedPort, uri.getPort());
+
+        // test headers
+        //
+        HttpHeaders headers = request.headers();
+        List<String> headersList = headers.allValues("Authorization");
+        Map<String, List<String>> allHeaders = headers.map();
+        assertEquals(expectedNumOfHeaders, allHeaders.size());
+        assertEquals(expectedNumOfAuthHeaderValue, headersList.size());
+        assertEquals(expectedAuthHeaderValue, headersList.get(0));
+
+    }
+
 }
